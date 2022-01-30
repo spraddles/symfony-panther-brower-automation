da8762a6c921cb3cf999acc6cb4fafe6173a66fd
brettspradbury <brett.spradbury@gmail.com>
2022-01-30 17:58:09 +1100

    mustache command
    sh ./mustache.sh

--- mustache.sh
+++ mustache.sh
@@ -4,6 +4,6 @@ for file in src/views/*.pine
 do
 	for ((i = 0; i < 3; i++))
 	do
-        mustache ./tradingview/templates/source.json ./tradingview/scripts/Strategy_1.pine > ./tradingview/Strategy_1.pine
+        mustache ./tradingview/templates/source.json ./tradingview/scripts/Strategy_6.pine > ./tradingview/Strategy_6.pine
 	done
 done
