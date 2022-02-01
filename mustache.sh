fac2a0a27cf007d5717ec4bc637c7a19efd6e91c
brettspradbury <brett.spradbury@gmail.com>
2022-02-02 08:57:14 +1100

    Add mustache templating
    install with NPM globally

--- mustache.sh
+++ mustache.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env node
 
+rm -r ./tradingview/output/*
+
+count=$(ls ./tradingview/scripts | wc -l)+1
+
 for file in src/views/*.pine
 do
-	for ((i = 0; i < 3; i++))
+	for ((i = 1; i < count; i++))
 	do
-        mustache ./tradingview/templates/source.json ./tradingview/scripts/Strategy_6.pine > ./tradingview/output/Strategy_6.pine
+        mustache ./tradingview/templates/source.json ./tradingview/scripts/Strategy_$i.pine > ./tradingview/output/Strategy_$i.pine
 	done
 done
