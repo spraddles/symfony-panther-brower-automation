4bdf6b4ddc8008812ea94ff722c03d1e0cbc9ca5
brettspradbury <brett.spradbury@gmail.com>
2022-02-02 22:50:57 +1100

    cater for alphanumeric filenames

--- mustache.sh
+++ mustache.sh
@@ -2,12 +2,9 @@
 
 rm -r ./tradingview/output/*
 
-count=$(ls ./tradingview/scripts | wc -l)+1
-
-for file in src/views/*.pine
-do
-	for ((i = 1; i < count; i++))
-	do
-        mustache ./tradingview/templates/source.json ./tradingview/scripts/Strategy_$i.pine > ./tradingview/output/Strategy_$i.pine
-	done
-done
+mustache ./tradingview/templates/source.json ./tradingview/scripts/Strategy_1.pine > ./tradingview/output/Strategy_1.pine
+mustache ./tradingview/templates/source.json ./tradingview/scripts/Strategy_2.pine > ./tradingview/output/Strategy_2.pine
+mustache ./tradingview/templates/source.json ./tradingview/scripts/Strategy_3.pine > ./tradingview/output/Strategy_3.pine
+mustache ./tradingview/templates/source.json ./tradingview/scripts/Strategy_4a.pine > ./tradingview/output/Strategy_4a.pine
+mustache ./tradingview/templates/source.json ./tradingview/scripts/Strategy_5.pine > ./tradingview/output/Strategy_5.pine
+mustache ./tradingview/templates/source.json ./tradingview/scripts/Strategy_6.pine > ./tradingview/output/Strategy_6.pine
